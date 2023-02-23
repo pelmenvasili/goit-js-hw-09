b7b3d56c6406864f152e20e81ff1c5532463f0cd
pelmenvasili <112347075+pelmenvasili@users.noreply.github.com>
2023-02-23 19:35:49 +0000

    Deploying to gh-pages from @ pelmenvasili/goit-js-hw-09@cc90d0ff8bdbd75653dfd6a0a5f93477e6f067f8 🚀

---
+++ 01-color-switcher.4e6835fd.js
@@ -0,0 +1,2 @@
+let t;const e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");e.addEventListener("click",(()=>{t=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(()=>{clearInterval(t),e.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.4e6835fd.js.map
